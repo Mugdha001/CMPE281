7bca3ddb6e82ca17a3c1ec22765eb9f8ac8720c0
Mugdha Wadikar <mugdhawadikar@gmail.com>
2019-02-21 18:46:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
-});
+});*/
+
+var http = require('http');
+
+var port = 9000;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello there, world!\n');
+}).listen(port);
+
+console.log('Listening on port', port);
