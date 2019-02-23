ff1a2b515fca0f218ac5a4164e4e70d9dda8cfe7
Mugdha Wadikar <mugdhawadikar@gmail.com>
2019-02-22 16:14:56 -0800

    Uploaded video

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-/*var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
-});*/
-
+//app.js program for Hello World Application
 var http = require('http');
 
 var port = 9000;
